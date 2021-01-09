b39e4ed794612b13c772c9fca2bb6404a5d005ed
Tamara <60240971+Prussak@users.noreply.github.com>
2021-01-09 14:34:18 -0300

    Add files via upload

---
+++ Programacao_Estruturada/Codigos/Programa.java
@@ -0,0 +1,23 @@
+public class Programa {
+	public static int varGlobal = 5;
+	private static int varLocalClasse = 6;
+	
+	public static void main(String []args){
+	int varLocalRotina = 10;
+
+	if (var Globaal == 0){
+int varLocalComando = varLocalRotina;
+System.out.Println(varLocalComando);
+}
+System.out.println(varLocalRotina);
+//System.out.println(varLocalComando);
+System.out.println(varGlobal);
+System.out.println(varLocalClasse);	
+}
+public static void rotina2(){
+//System.out.println(varLocalRotina);
+//System.out.println(varLocalComando);
+System.out.println(varGlobal);
+System.out.println(varLocalClasse);	
+}	
+}
---
+++ Programacao_Estruturada/Codigos/ProgramaPrintln.java
@@ -0,0 +1,10 @@
+public class ProgramaPrintln{
+public static void mais(String[]args){
+int x = 10.
+double y = 10.2;
+System.out.println("Esta mensagem serpa impressa na tela!");
+System.out.println("O valor da variável x é" + x);
+System.out.println("Os valores de x e y são " +x+ "e" +y);
+System.out.println("Linha 1\nLinha2");
+}
+}
